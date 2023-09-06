f194b68680c44991d939dbbf1a9b019d5b69b0c5
fatima88006 <mybagisnice@gmail.com>
2023-09-06 19:49:55 +0300

    cw6

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# write your code here
+person = {
+        "name": "Fatima",
+        "age": 17,
+        "hobbies": ["reading books" , "watching tv" , "playing sport"]
+}
+
+print(person.get("name"))
+print(person.get("age"))
+
+person["age"]=18
+person['country'] = "Spain"
+print(person)
+print (len(person))
+person["hobbies"].append ("playing video gams")
+print(person)
+def check_hobbies(person):
+    if len(person["hobbies"]) >= 3 :
+        return"wow you are amazing"
+    else:
+         return "put more if you can"
+print(check_hobbies(person))
